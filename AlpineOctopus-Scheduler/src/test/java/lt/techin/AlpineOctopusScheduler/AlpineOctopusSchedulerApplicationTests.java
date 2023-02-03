7dd46bdc50c1694202e166ac459ad507dc15a99a
Mantas Povilas Bagociunas <mpovilasb@live.com>
2023-02-03 15:48:06 +0200

    initial commit

---
+++ AlpineOctopus-Scheduler/src/test/java/lt/techin/AlpineOctopusScheduler/AlpineOctopusSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.AlpineOctopusScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpineOctopusSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
