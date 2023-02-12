158b2c96a77832cf9315ddce7448745967734767
Mantvydas Juršys <mantvydas.jursys@stud.vtmc.lt>
2023-02-12 18:46:56 +0200

    added package(repository) and created TeacherService in package(service)

---
+++ AlpineOctopus-Scheduler/src/main/java/lt/techin/AlpineOctopusScheduler/repository/TeacherRepository.java
@@ -0,0 +1,7 @@
+package lt.techin.AlpineOctopusScheduler.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TeacherRepository {
+}
---
+++ AlpineOctopus-Scheduler/src/main/java/lt/techin/AlpineOctopusScheduler/service/TeacherService.java
@@ -0,0 +1,8 @@
+package lt.techin.AlpineOctopusScheduler.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeacherService {
+    
+}
