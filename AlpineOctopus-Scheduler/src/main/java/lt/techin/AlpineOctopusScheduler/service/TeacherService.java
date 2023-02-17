602caf511bbb0d1e9bfa28d3d20bc6a93766820d
Mantas Povilas Bagociunas <mpovilasb@live.com>
2023-02-17 15:51:58 +0200

    teachersList

--- AlpineOctopus-Scheduler/src/main/java/lt/techin/AlpineOctopusScheduler/service/TeacherService.java
+++ AlpineOctopus-Scheduler/src/main/java/lt/techin/AlpineOctopusScheduler/service/TeacherService.java
@@ -1,11 +1,20 @@
 package lt.techin.AlpineOctopusScheduler.service;
 
+import lt.techin.AlpineOctopusScheduler.api.dto.GroupsEntityDto;
+import lt.techin.AlpineOctopusScheduler.api.dto.TeacherEntityDto;
+import lt.techin.AlpineOctopusScheduler.api.dto.mapper.GroupsMapper;
+import lt.techin.AlpineOctopusScheduler.api.dto.mapper.TeacherMapper;
 import lt.techin.AlpineOctopusScheduler.dao.TeacherRepository;
+import lt.techin.AlpineOctopusScheduler.model.Groups;
 import lt.techin.AlpineOctopusScheduler.model.Teacher;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 //Mantvydas Juršys
 
@@ -30,7 +39,16 @@ public class TeacherService {
         return teacherRepository.findById(id);
     }
 
-    
+    public List<Teacher> getPagedAllTeachers(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        return teacherRepository.findAll(pageable).stream().collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<TeacherEntityDto> getTeachersByNameContaining(String nameText) {
+        return teacherRepository.findByNameContainingIgnoreCase(nameText).stream()
+                .map(TeacherMapper::toTeacherEntityDto).collect(Collectors.toList());
+    }
 
     public Teacher create(Teacher teacher) {
 
--- AlpineOctopus-Scheduler/src/main/resources/db/migration/V3_6__insert_teacher_test_data.sql
+++ AlpineOctopus-Scheduler/src/main/resources/db/migration/V3_6__insert_teacher_test_data.sql
@@ -1,9 +1,6 @@
-INSERT INTO TEACHER (NAME,SURNAME,LOGIN_EMAIL,
-                     CONTACT_EMAIL, PHONE,
-                     WORK_HOURS_PER_WEEK,
-                     SHIFT) VALUES
+INSERT INTO TEACHER (NAME,SURNAME,LOGIN_EMAIL,CONTACT_EMAIL, PHONE, WORK_HOURS_PER_WEEK,SHIFT, created_date, modified_date, created_by, modified_by) VALUES
 
-('Mantvydas', 'Juršys', 'mantvydas@loginEmail.lt','mantvydas@contactEmail.lt', '+370 123 321 12', 40, 'Rytas'),
-('Tomas', 'Tomauskas', 'tomas@loginEmail.lt','tomas@contactEmail.lt', '+370 678 321 12', 30, 'Vakaras'),
-('Lukas', 'Lukauskis', 'lukauskis@loginEmail.lt','lukauskis@contactEmail.lt', '+370 123 345 12', 20, 'Rytas'),
-('Justas', 'Justinas', 'justis@loginEmail.lt','justelis@contactEmail.lt', '+370 789 321 12', 50, 'Rytas');
+('Mantvydas', 'Juršys', 'mantvydas@loginEmail.lt','mantvydas@contactEmail.lt', '+370 123 321 12', 40, 'Rytas','2023-02-08 00:36:54.344',null,'Mantas', 'Mantas' ),
+('Tomas', 'Tomauskas', 'tomas@loginEmail.lt','tomas@contactEmail.lt', '+370 678 321 12', 30, 'Vakaras','2023-02-08 00:36:54.344',null,'Mantas', 'Mantas' ),
+('Lukas', 'Lukauskis', 'lukauskis@loginEmail.lt','lukauskis@contactEmail.lt', '+370 123 345 12', 20, 'Rytas','2023-02-08 00:36:54.344',null,'Mantas', 'Mantas' ),
+('Justas', 'Justinas', 'justis@loginEmail.lt','justelis@contactEmail.lt', '+370 789 321 12', 50, 'Rytas','2023-02-08 00:36:54.344',null,'Mantas', 'Mantas' );
