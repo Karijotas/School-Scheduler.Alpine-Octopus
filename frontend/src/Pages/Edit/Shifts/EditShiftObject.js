1a84eddc15396e620a92bdb0c340cd36077f4fb9
Mantas Povilas Bagociunas <mpovilasb@live.com>
2023-03-03 10:41:45 +0200

    shift edit

--- frontend/src/Pages/Edit/Shifts/EditShiftObject.js
+++ frontend/src/Pages/Edit/Shifts/EditShiftObject.js
@@ -25,61 +25,61 @@ export function EditShiftObject() {
     ends: "",
   });
 
-    const [name, setName] = useState("");
-    const [starts, setStarts] = useState("");
-    const [ends, setEnds] = useState("");
+  const [name, setName] = useState("");
+  const [starts, setStarts] = useState("");
+  const [ends, setEnds] = useState("");
 
-    const [nameError, setNameError] = useState("")
-    const [startError, setStartError] = useState("")
-    const [endError, setEndError] = useState("")
+  const [nameError, setNameError] = useState("")
+  const [startError, setStartError] = useState("")
+  const [endError, setEndError] = useState("")
 
-    const [formValid, setFormValid] = useState(false)
+  const [formValid, setFormValid] = useState(false)
 
 
-    useEffect(() => {
-        if(nameError || startError || endError) {
-          setFormValid(false)
-        } else {
-          setFormValid(true)
-        }
-      }, [nameError, startError, endError])
+  useEffect(() => {
+    if (nameError || startError || endError) {
+      setFormValid(false)
+    } else {
+      setFormValid(true)
+    }
+  }, [nameError, startError, endError])
 
-    const handleNameInputChange = (e) => {
-        shifts.name = e.target.value
-        setName(e.target.value);
-        validateNameInput(e.target.value);
-      };
+  const handleNameInputChange = (e) => {
+    shifts.name = e.target.value
+    setName(e.target.value);
+    validateNameInput(e.target.value);
+  };
 
-    const handleStartInputChange = (e) => {
-        shifts.starts = e.target.value
-        setStarts(e.target.value);
-        validateStartInput(e.target.value);
-      };
+  const handleStartInputChange = (e) => {
+    shifts.starts = e.target.value
+    setStarts(e.target.value);
+    validateStartInput(e.target.value);
+  };
 
-    const handleEndInputChange = (e) => {
-        shifts.ends = e.target.value
-        setEnds(e.target.value);
-        validateEndInput(e.target.value);
-      };
-    
-    const validateNameInput = (value) => {
-        if (value.length <2 || value.length > 40) {
-            setNameError("Įveskite nuo 2 iki 40 simbolių!")
-            if(!value){
-                setNameError("Pavadinimas negali būti tuščias!")
-              } 
-        } else {
-            setNameError("")
-        }
-      };
+  const handleEndInputChange = (e) => {
+    shifts.ends = e.target.value
+    setEnds(e.target.value);
+    validateEndInput(e.target.value);
+  };
 
-      const validateStartInput = (value) => {
-        
+  const validateNameInput = (value) => {
+    if (value.length < 2 || value.length > 40) {
+      setNameError("Įveskite nuo 2 iki 40 simbolių!")
+      if (!value) {
+        setNameError("Pavadinimas negali būti tuščias!")
+      }
+    } else {
+      setNameError("")
+    }
+  };
+
+  const validateStartInput = (value) => {
+
     setStarts(value);
-    
+
     if (!/^\d+$/.test(value)) {
       setStartError("Įveskite tik skaičius!");
-      
+
       if (!value) {
         setStartError("Negali būti tuščias!");
       }
@@ -87,22 +87,22 @@ export function EditShiftObject() {
       setStartError("Skaičius turi būti tarp 1 ir 14!");
     } else if (Number(ends) && Math.abs(value - Number(ends)) > 11) {
       setStartError("Intervalo skirtumas negali būti didesnis nei 12!");
-    } else if(value > Number(ends)){
+    } else if (Number(ends) && value > Number(ends)) {
       setStartError("Pamokos negali baigtis vėliau nei prasidėjo")
       setEndError("");
     } else {
       setStartError("");
     }
-      };
+  };
 
-      const validateEndInput = (value) => {
-        
+  const validateEndInput = (value) => {
+
     setEnds(value);
-    
-    
+
+
     if (!/^\d+$/.test(value)) {
       setEndError("Įveskite tik skaičius!");
-      
+
       if (!value) {
         setEndError("Negali būti tuščias!");
       }
@@ -110,13 +110,13 @@ export function EditShiftObject() {
       setEndError("Skaičius turi būti tarp 1 ir 14!");
     } else if (Number(starts) && Math.abs(Number(starts) - value) > 11) {
       setEndError("Intervalo skirtumas negali būti didesnis nei 12!");
-    } else if (Number(starts)>value){
+    } else if (Number(starts) > value) {
       setEndError("Pamokos negali baigtis anksčiau nei prasidėjo!")
       setStartError("");
     } else {
       setEndError("");
     }
-      };
+  };
 
   useEffect(() => {
     fetch("/api/v1/shifts/" + params.id)
@@ -228,23 +228,23 @@ export function EditShiftObject() {
                   <Table.Body>
                     <Table.Row>
                       <Table.Cell collapsing>
-                      {(nameError) && <div style={{color: "red"}}>{nameError}</div>}
+                        {(nameError) && <div style={{ color: "red" }}>{nameError}</div>}
                         <Input
                           value={shifts.name}
                           onChange={(e) => handleNameInputChange(e)}
                         />
                       </Table.Cell>
                       <Table.Cell collapsing>
-                      {(startError) && <div style={{color: "red"}}>{startError}</div>}
+                        {(startError) && <div style={{ color: "red" }}>{startError}</div>}
                         <Input
-                          value={shifts.starts} 
+                          value={shifts.starts}
                           onChange={(e) => handleStartInputChange(e)}
                         />
                       </Table.Cell>
                       <Table.Cell collapsing>
-                      {(endError) && <div style={{color: "red"}}>{endError}</div>}
+                        {(endError) && <div style={{ color: "red" }}>{endError}</div>}
                         <Input
-                          value={shifts.ends} 
+                          value={shifts.ends}
                           onChange={(e) => handleEndInputChange(e)}
                         />
                       </Table.Cell>
